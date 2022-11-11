67a8e8fa7129dd37c66bd347a220fa3796cb8093
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-11-11 22:16:02 +0300

    ReactNative Navigasyon Notlarım
    ReactNative Navigasyon Notlarım medium yazısı için.

--- App.js
+++ App.js
@@ -1,73 +1,45 @@
 import * as React from 'react';
-import {Text, View} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Icons from 'react-native-vector-icons/Ionicons';
-function HomeScreen() {
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+//Ana sayfa kullanıcının karşısına çıkan ilk sayfa.
+function HomeScreen({navigation}) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>KubilayBzk.dev Ana Sayfa!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text style={{fontSize:18,marginBottom:12}}>KubilayBzk.dev  'e hoşgeldiniz</Text>
+      <View style={{display:'flex', flexDirection:"row",justifyContent:"space-around"}}>
+      <Button onPress={()=>navigation.navigate("HomeDetails")} title="Giriş Yap"/>
+      </View>
     </View>
   );
 }
 
-function SettingsScreen() {
+function HomeDetails({ navigation }) {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>KubilayBzk.dev Ayarlar!</Text>
-    </View>
-  );
-}
-function Message() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>KubilayBzk.dev Mesajlar!</Text>
-    </View>
-  );
-}
-function Notification() {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>KubilayBzk.dev Bildirimler!</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+     <View style={{display:'flex',flexDirection:"column",justifyContent:'space-around'}}>
+     <Button onPress={()=>navigation.push("HomeDetails")} title="Detay"/>
+      <Button onPress={()=>navigation.goBack()} title="Geri Dön"/>
+      <Button onPress={()=>navigation.popToTop()} title="İlk sayfaya dön"/>
+     </View>
     </View>
   );
 }
 
-const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({route}) => ({
-          //Her bir route elemanına erişmek için mapliyoruz.
-          tabBarIcon: ({focused, color, size}) => {
-            //Burada her bir elamanın koşullara göre nasıl olması gerektiğini oluşturuyoruz.
-            let iconName; //Icon ismi koşula göre değiştiği için gerekli olan değişken.
-            if (route.name === 'Home') {
-              iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Settings') {
-              iconName = focused ? 'settings' : 'settings-outline';
-            } else if (route.name === 'Notification') {
-              iconName = focused
-                ? 'md-notifications-sharp'
-                : 'md-notifications-outline';
-            } else if (route.name === 'Message') {
-              iconName = focused ? 'md-mail-open' : 'md-mail-open-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Icons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-          headerShown: false,
-        })}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Notification" component={Notification} />
-        <Tab.Screen name="Message" component={Message} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
-      </Tab.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home"  component={HomeScreen} />
+        <Stack.Screen name="HomeDetails" component={HomeDetails} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
