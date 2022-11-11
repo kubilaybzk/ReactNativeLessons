533d2f0d5254dd8600ea7549c26f0d90ba3f649d
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-11-11 23:50:47 +0300

    Router kullanarak parametre geçme.
    Router kullanarak parametre geçme.

--- App.js
+++ App.js
@@ -1,30 +1,50 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//Gönderilecek bir obje olsun.
+let FirstObj = {
+  itemId: 86,
+  otherParam: {
+    name: 'Kubilay',
+  },
+};
+
 
 
 //Ana sayfa kullanıcının karşısına çıkan ilk sayfa.
 function HomeScreen({navigation}) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style={{fontSize:18,marginBottom:12}}>KubilayBzk.dev  'e hoşgeldiniz</Text>
-      <View style={{display:'flex', flexDirection:"row",justifyContent:"space-around"}}>
-      <Button onPress={()=>navigation.navigate("HomeDetails")} title="Giriş Yap"/>
-      </View>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => {
+          /* 1. Navigate to the Details route with params */
+          navigation.navigate('Details', FirstObj);
+        }}
+      />
     </View>
   );
 }
 
-function HomeDetails({ navigation }) {
+function DetailsScreen({route, navigation}) {
+  /* 2. Get the param */
+  const {itemId, otherParam} = route.params;
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-     <View style={{display:'flex',flexDirection:"column",justifyContent:'space-around'}}>
-     <Button onPress={()=>navigation.push("HomeDetails")} title="Detay"/>
-      <Button onPress={()=>navigation.goBack()} title="Geri Dön"/>
-      <Button onPress={()=>navigation.popToTop()} title="İlk sayfaya dön"/>
-     </View>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details', {
+          itemId: Math.floor(Math.random() * 100),
+          otherParam: {name: 'Ali' + Math.floor(Math.random() * 100)},
+        })}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
     </View>
   );
 }
@@ -35,8 +55,8 @@ function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home"  component={HomeScreen} />
-        <Stack.Screen name="HomeDetails" component={HomeDetails} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
